f4f465c00104e29add59cadba0b516821ab0d13c
Virginia García <vickkygm1@gmail.com>
2018-02-28 12:46:39 -0600

    j

--- Cookies/Python/setup.py
+++ Cookies/Python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cookies',
-      version='0.3',
+      version='0.4',
       description='Cool person or not',
       url='https://github.com/vickygm/cookies',
       author='Moana',
